0bfc4cff51c8c6432395a5ac0475d43f365821af
Nikita <nikita@Nikitas-MacBook-Air.local>
2023-07-18 13:05:34 +0300

    server db

--- web/src/static/js/modules/state.js
+++ web/src/static/js/modules/state.js
@@ -3,7 +3,7 @@
 
 // let addr = process.env.CHECK
 // console.log(addr)
-export let appAddr = 'http://172.20.10.7:8182'
+export let appAddr = 'http://192.168.1.231:8182'
 const userInf = JSON.parse(sessionStorage.getItem('user'))
 
 let state = {
