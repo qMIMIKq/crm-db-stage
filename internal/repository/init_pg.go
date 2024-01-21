9937549712cb68ba5fb346ebf24162a288f6949b
Никита Брезицкий <nikita@Nikitas-MacBook-Air.local>
2024-01-21 15:10:38 +0300

    update init

--- internal/repository/init_pg.go
+++ internal/repository/init_pg.go
@@ -51,7 +51,7 @@ func (i InitPG) InitDB() error {
 				plot_id   SERIAL UNIQUE PRIMARY KEY NOT NULL,
 				plot_name VARCHAR(255)              NOT NULL,
 				nickname  varchar(255) DEFAULT '',
-			  disable     boolean      default false
+			    disable     boolean      default false
 		);
 		
 		INSERT INTO plots (plot_name, nickname)
@@ -82,7 +82,8 @@ func (i InitPG) InitDB() error {
 				login     VARCHAR(255),
 				password  VARCHAR(255),
 				nickname  VARCHAR(255),
-				disable   boolean DEFAULT FALSE
+				disable   boolean DEFAULT FALSE,
+				general boolean DEFAULT FALSE
 		);
 		
 		CREATE TABLE IF NOT EXISTS users_rights
@@ -106,47 +107,54 @@ func (i InitPG) InitDB() error {
 		
 		CREATE table IF NOT EXISTS orders
 		(
-				order_id        SERIAL UNIQUE PRIMARY KEY NOT NULL,
-				order_timestamp timestamptz  default CURRENT_TIMESTAMP::timestamptz,
-				time_of_modify  timestamp,
-				order_number    varchar(255),
-				order_sample    varchar(255),
-				order_client    varchar(255),
-				order_name      varchar(255),
-				order_material  varchar(255),
-				order_quantity  varchar(255),
-				order_issued    varchar(255) default '',
-				order_m         varchar(255) default '',
-				order_endtime   date                      null,
-				order_otk       varchar(255),
-				order_p         varchar(255),
-				completed       boolean      default false
+			order_id        SERIAL UNIQUE PRIMARY KEY NOT NULL,
+			order_timestamp timestamp with time zone DEFAULT CURRENT_TIMESTAMP::timestamptz,
+			order_number    VARCHAR(255),
+			order_sample    VARCHAR(255),
+			order_client    VARCHAR(255),
+			order_name      VARCHAR(255),
+			order_material  VARCHAR(255),
+			order_quantity  VARCHAR(255),
+			order_issued    VARCHAR(255)             DEFAULT '',
+			order_m         VARCHAR(255)             DEFAULT '',
+			order_endtime   DATE                      null,
+			order_otk       VARCHAR(255),
+			order_p         VARCHAR(255),
+			completed       boolean                  DEFAULT false,
+			time_of_modify  timestamp without time zone
 		);
 		
 		CREATE TABLE IF NOT EXISTS routes
 		(
-				route_id          SERIAL UNIQUE PRIMARY KEY NOT NULL,
-				route_position    int,
-				plot_id           varchar(255),
-				order_id          int references orders (order_id) on delete cascade,
-				worker            varchar(255),
-				quantity          varchar(255),
-				issued            varchar(255),
-				start_time        varchar(255),
-				end_time          varchar(255),
-				pause_time        varchar(255),
-				pause_value       text         default '',
-				error_time        varchar(255),
-				error_value       varchar(255),
-				day_quantity      varchar(255) default '',
-				theor_end         varchar(255) default '',
-				dyn_end           varchar(255) default '',
-				plan_date         varchar(255) default '',
-				plan_start        varchar(255) default '',
-				plan_exclude_days text         default '',
-				plan_faster       boolean      default false,
-				last_comment      text         default '',
-				plan_dates        text         default ''
+			route_id          SERIAL UNIQUE PRIMARY KEY NOT NULL,
+			route_position    INT,
+			plot_id           VARCHAR(255),
+			order_id          INT REFERENCES orders (order_id) on delete cascade,
+			worker            VARCHAR(255),
+			quantity          VARCHAR(255),
+			issued            VARCHAR(255),
+			start_time        VARCHAR(255),
+			end_time          VARCHAR(255),
+			pause_time        VARCHAR(255),
+			pause_value       text         DEFAULT '',
+			error_time        VARCHAR(255),
+			error_value       VARCHAR(255),
+			day_quantity      VARCHAR(255) DEFAULT '',
+			theor_end         VARCHAR(255) DEFAULT '',
+			dyn_end           VARCHAR(255) DEFAULT '',
+			plan_date         VARCHAR(255) DEFAULT '',
+			plan_start        VARCHAR(255) DEFAULT '',
+			plan_exclude_days text         DEFAULT '',
+			plan_faster       boolean      DEFAULT false,
+			last_comment      text         DEFAULT '',
+			issued_plan       VARCHAR(255) DEFAULT '',
+			plan_dates        text         DEFAULT '',
+			planned boolean default false,
+			time              real         DEFAULT 0,
+			up                INT          DEFAULT 0,
+			adjustment        INT          DEFAULT 0,
+			need_shifts       INT,
+			shift             VARCHAR(255) DEFAULT ''
 		);
 		
 		create table IF NOT EXISTS route_comments
@@ -190,49 +198,57 @@ func (i InitPG) InitDB() error {
 		
 		CREATE TABLE IF NOT EXISTS reports
 		(
-				report_id      serial unique PRIMARY KEY NOT NULL,
-				report_date    date,
-				order_id       int,
-				order_number   varchar(255),
-				order_client   varchar(255),
-				order_name     varchar(255),
-				order_material varchar(255),
-				order_plot     varchar(255),
-				adding_date    date,
-				quantity       varchar(255),
-				issued         varchar(255),
-				plan           varchar(255),
-				operator       varchar(255),
-				issued_plan    varchar(255),
-				route_position varchar(255) default '0',
-				route_id       integer      default 0
+			report_id       serial unique PRIMARY KEY NOT NULL,
+			report_date     DATE,
+			order_id        INT,
+			order_number    VARCHAR(255),
+			order_client    VARCHAR(255),
+			order_name      VARCHAR(255),
+			order_material  VARCHAR(255),
+			order_plot      VARCHAR(255),
+			adding_date     DATE,
+			quantity        VARCHAR(255),
+			issued          VARCHAR(255),
+			plan            VARCHAR(255),
+			operator        VARCHAR(255),
+			issued_plan     VARCHAR(255),
+			route_position  VARCHAR(255) DEFAULT '0',
+			route_id        INT,
+			order_timestamp DATE,
+			need_shifts     INT          DEFAULT 0,
+			shift           VARCHAR(255) DEFAULT '',
+			not_planned     boolean      DEFAULT false,
+			current_shift   INT          DEFAULT 0,
+			adjustment      VARCHAR(255) DEFAULT ''
 		);
 		
 		create table IF NOT EXISTS plans
 		(
-				plan_id    serial unique PRIMARY KEY NOT NULL,
-				route_id   integer      default 0,
-				order_id   integer      default 0,
-				route_plot varchar(255) default '',
-				plan_date  date,
-				divider    varchar(255),
-				queues     varchar(255)
+			plan_id    serial unique PRIMARY KEY NOT NULL,
+			route_id   INT REFERENCES routes (route_id) on update cascade on delete cascade,
+			order_id   INT REFERENCES orders (order_id) on update cascade on delete cascade,
+			route_plot VARCHAR(255) DEFAULT '',
+			plan_date  DATE,
+			divider    VARCHAR(255),
+			queues     VARCHAR(255)
 		);
 
 		create table planning
 		(
 			planning_id     serial unique PRIMARY KEY NOT NULL,
-			route_id        integer      default 0,
-			route_plot      varchar(255) default '',
-			order_id        varchar(255),
+			route_id        INT REFERENCES routes (route_id) on update cascade on delete cascade,
+			route_plot      VARCHAR(255) DEFAULT '',
+			order_id        INT REFERENCES orders (order_id) on update cascade on delete cascade,
 			order_timestamp timestamptz,
 			time_of_modify  timestamp,
-			order_number    varchar(255),
-			order_client    varchar(255),
-			order_name      varchar(255),
-			order_material  varchar(255),
-			order_quantity  varchar(255),
-			order_issued    varchar(255) default ''
+			order_number    VARCHAR(255),
+			order_client    VARCHAR(255),
+			order_name      VARCHAR(255),
+			order_material  VARCHAR(255),
+			order_quantity  VARCHAR(255),
+			order_issued    VARCHAR(255) DEFAULT '',
+			need_shifts     integer      DEFAULT 0,
+			position        VARCHAR(255) DEFAULT ''
 		);
 	`)
 
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -3,7 +3,7 @@ SET timezone = 'Europe/Moscow';
 CREATE TABLE groups
 (
     group_id   SERIAL UNIQUE PRIMARY KEY NOT NULL,
-    group_name varchar(255)
+    group_name VARCHAR(255)
 );
 
 INSERT INTO groups (group_name)
@@ -16,11 +16,11 @@ VALUES ('супер-админ'),
 CREATE TABLE groups_description
 (
     description_id serial unique primary key                          not null,
-    group_id       int references groups (group_id) ON DELETE CASCADE NOT NULL,
-    description    varchar(255) default 'Нет данных'
+    group_id       INT REFERENCES groups (group_id) ON DELETE CASCADE NOT NULL,
+    description    VARCHAR(255) DEFAULT 'Нет данных'
 );
 
-insert into groups_description (group_id, description)
+insert INTo groups_description (group_id, description)
 VALUES (1, 'супер админ'),
        (2, 'админ'),
        (3, 'технолог'),
@@ -31,7 +31,7 @@ CREATE TABLE plots
 (
     plot_id   SERIAL UNIQUE PRIMARY KEY NOT NULL,
     plot_name VARCHAR(255)              NOT NULL,
-    nickname  varchar(255) DEFAULT ''
+    nickname  VARCHAR(255) DEFAULT ''
 );
 
 INSERT INTO plots (plot_name)
@@ -45,11 +45,11 @@ CREATE TABLE filters
     filter_id   SERIAL UNIQUE PRIMARY KEY                        NOT NULL,
     filter_name VARCHAR(255)                                     NOT NULL,
     plot_id     INT REFERENCES plots (plot_id) ON DELETE CASCADE NOT NULL,
-    start_time  varchar(255) default '08:20',
-    end_time    varchar(255) default '20:00',
-    nickname    varchar(255) default '',
-    position    integer      default 0,
-    disable     boolean      default false
+    start_time  VARCHAR(255) DEFAULT '08:20',
+    end_time    VARCHAR(255) DEFAULT '20:00',
+    nickname    VARCHAR(255) DEFAULT '',
+    position    INTeger      DEFAULT 0,
+    disable     boolean      DEFAULT false
 );
 
 CREATE TABLE users
@@ -66,7 +66,7 @@ CREATE TABLE users
 CREATE TABLE users_rights
 (
     right_id SERIAL UNIQUE PRIMARY KEY                        NOT NULL,
-    user_id  int references users (user_id) ON DELETE CASCADE NOT NULL,
+    user_id  INT REFERENCES users (user_id) ON DELETE CASCADE NOT NULL,
     group_id INT REFERENCES groups (group_id)                 NOT NULL,
     plot_id  INT REFERENCES plots (plot_id)                   NOT NULL
 );
@@ -85,65 +85,69 @@ VALUES (1, 1, 1);
 CREATE table orders
 (
     order_id        SERIAL UNIQUE PRIMARY KEY NOT NULL,
-    order_timestamp timestamptz  default CURRENT_TIMESTAMP::timestamptz,
-    time_of_modify  timestamp,
-    order_number    varchar(255),
-    order_sample    varchar(255),
-    order_client    varchar(255),
-    order_name      varchar(255),
-    order_material  varchar(255),
-    order_quantity  varchar(255),
-    order_issued    varchar(255) default '',
-    order_m         varchar(255) default '',
-    order_endtime   date                      null,
-    order_otk       varchar(255),
-    order_p         varchar(255),
-    completed       boolean      default false
+    order_timestamp timestamp with time zone DEFAULT CURRENT_TIMESTAMP::timestamptz,
+    order_number    VARCHAR(255),
+    order_sample    VARCHAR(255),
+    order_client    VARCHAR(255),
+    order_name      VARCHAR(255),
+    order_material  VARCHAR(255),
+    order_quantity  VARCHAR(255),
+    order_issued    VARCHAR(255)             DEFAULT '',
+    order_m         VARCHAR(255)             DEFAULT '',
+    order_endtime   DATE                      null,
+    order_otk       VARCHAR(255),
+    order_p         VARCHAR(255),
+    completed       boolean                  DEFAULT false,
+    time_of_modify  timestamp without time zone
 );
 
 CREATE TABLE routes
 (
     route_id          SERIAL UNIQUE PRIMARY KEY NOT NULL,
-    route_position    int,
-    plot_id           varchar(255),
-    order_id          int references orders (order_id) on delete cascade,
-    worker            varchar(255),
-    quantity          varchar(255),
-    issued            varchar(255),
-    start_time        varchar(255),
-    end_time          varchar(255),
-    pause_time        varchar(255),
-    pause_value       text         default '',
-    error_time        varchar(255),
-    error_value       varchar(255),
-    day_quantity      varchar(255) default '',
-    theor_end         varchar(255) default '',
-    dyn_end           varchar(255) default '',
-    plan_date         varchar(255) default '',
-    plan_start        varchar(255) default '',
-    plan_exclude_days text         default '',
-    plan_faster       boolean      default false,
-    last_comment      text         default '',
-    plan_dates        text         default '',
-    time              int          default 0,
-    up                int          default 0,
-    adjustment        int          default 0
+    route_position    INT,
+    plot_id           VARCHAR(255),
+    order_id          INT REFERENCES orders (order_id) on delete cascade,
+    worker            VARCHAR(255),
+    quantity          VARCHAR(255),
+    issued            VARCHAR(255),
+    start_time        VARCHAR(255),
+    end_time          VARCHAR(255),
+    pause_time        VARCHAR(255),
+    pause_value       text         DEFAULT '',
+    error_time        VARCHAR(255),
+    error_value       VARCHAR(255),
+    day_quantity      VARCHAR(255) DEFAULT '',
+    theor_end         VARCHAR(255) DEFAULT '',
+    dyn_end           VARCHAR(255) DEFAULT '',
+    plan_date         VARCHAR(255) DEFAULT '',
+    plan_start        VARCHAR(255) DEFAULT '',
+    plan_exclude_days text         DEFAULT '',
+    plan_faster       boolean      DEFAULT false,
+    last_comment      text         DEFAULT '',
+    issued_plan       VARCHAR(255) DEFAULT '',
+    plan_dates        text         DEFAULT '',
+    planned           boolean      default false,
+    time              real         DEFAULT 0,
+    up                INT          DEFAULT 0,
+    adjustment        INT          DEFAULT 0,
+    need_shifts       INT,
+    shift             VARCHAR(255) DEFAULT ''
 );
 
 create table route_comments
 (
     comment_id serial unique primary key not null,
-    route_id   int references routes (route_id) on update cascade on delete cascade,
-    date       varchar(255),
-    value      varchar(255)
+    route_id   INT REFERENCES routes (route_id) on update cascade on delete cascade,
+    DATE       VARCHAR(255),
+    value      VARCHAR(255)
 );
 
 create table route_issued
 (
     issued_id serial unique primary key not null,
-    route_id  int references routes (route_id) on update cascade on delete cascade,
-    date      varchar(255),
-    value     varchar(255)
+    route_id  INT REFERENCES routes (route_id) on update cascade on delete cascade,
+    DATE      VARCHAR(255),
+    value     VARCHAR(255)
 );
 
 INSERT INTO orders (order_number,
@@ -165,54 +169,61 @@ create table comments
 (
     comment_id     SERIAL UNIQUE PRIMARY KEY NOT NULL,
     comment_text   TEXT,
-    comment_author varchar(255),
+    comment_author VARCHAR(255),
     order_id       INT REFERENCES orders (order_id) ON DELETE CASCADE
 );
 
 CREATE TABLE reports
 (
     report_id       serial unique PRIMARY KEY NOT NULL,
-    report_date     date,
+    report_date     DATE,
     order_id        INT,
-    order_number    varchar(255),
-    order_client    varchar(255),
-    order_name      varchar(255),
-    order_material  varchar(255),
-    order_plot      varchar(255),
-    adding_date     date,
-    quantity        varchar(255),
-    issued          varchar(255),
-    plan            varchar(255),
-    operator        varchar(255),
-    issued_plan     varchar(255),
-    route_position  varchar(255) default '0',
-    route_id        int,
-    order_timestamp date
+    order_number    VARCHAR(255),
+    order_client    VARCHAR(255),
+    order_name      VARCHAR(255),
+    order_material  VARCHAR(255),
+    order_plot      VARCHAR(255),
+    adding_date     DATE,
+    quantity        VARCHAR(255),
+    issued          VARCHAR(255),
+    plan            VARCHAR(255),
+    operator        VARCHAR(255),
+    issued_plan     VARCHAR(255),
+    route_position  VARCHAR(255) DEFAULT '0',
+    route_id        INT,
+    order_timestamp DATE,
+    need_shifts     INT          DEFAULT 0,
+    shift           VARCHAR(255) DEFAULT '',
+    not_planned     boolean      DEFAULT false,
+    current_shift   INT          DEFAULT 0,
+    adjustment      VARCHAR(255) DEFAULT ''
 );
 
 create table plans
 (
     plan_id    serial unique PRIMARY KEY NOT NULL,
-    route_id   int references routes (route_id) on update cascade on delete cascade,
-    order_id   int references orders (order_id) on update cascade on delete cascade,
-    route_plot varchar(255) default '',
-    plan_date  date,
-    divider    varchar(255),
-    queues     varchar(255)
+    route_id   INT REFERENCES routes (route_id) on update cascade on delete cascade,
+    order_id   INT REFERENCES orders (order_id) on update cascade on delete cascade,
+    route_plot VARCHAR(255) DEFAULT '',
+    plan_date  DATE,
+    divider    VARCHAR(255),
+    queues     VARCHAR(255)
 );
 
 create table planning
 (
     planning_id     serial unique PRIMARY KEY NOT NULL,
-    route_id        int references routes (route_id) on update cascade on delete cascade,
-    route_plot      varchar(255) default '',
-    order_id        int references orders (order_id) on update cascade on delete cascade,
+    route_id        INT REFERENCES routes (route_id) on update cascade on delete cascade,
+    route_plot      VARCHAR(255) DEFAULT '',
+    order_id        INT REFERENCES orders (order_id) on update cascade on delete cascade,
     order_timestamp timestamptz,
     time_of_modify  timestamp,
-    order_number    varchar(255),
-    order_client    varchar(255),
-    order_name      varchar(255),
-    order_material  varchar(255),
-    order_quantity  varchar(255),
-    order_issued    varchar(255) default ''
+    order_number    VARCHAR(255),
+    order_client    VARCHAR(255),
+    order_name      VARCHAR(255),
+    order_material  VARCHAR(255),
+    order_quantity  VARCHAR(255),
+    order_issued    VARCHAR(255) DEFAULT '',
+    need_shifts     integer      DEFAULT 0,
+    position        VARCHAR(255) DEFAULT ''
 );
